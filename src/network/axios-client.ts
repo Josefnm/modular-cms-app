bf46a2c0a3055eddae8be95d864b458ceabbe399
josef <josefnm86@gmail.com>
2020-05-04 14:31:18 +0200

    fixed url path

--- src/network/axios-client.ts
+++ src/network/axios-client.ts
@@ -3,32 +3,23 @@ import * as firebase from 'firebase/app'
 import 'firebase/auth'
 
 export const client = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: `${process.env.REACT_APP_API_URL}/api`,
   timeout: 5000,
   headers: { 'Content-type': 'application/json' },
 })
 
 export const clientNoAuth = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: `${process.env.REACT_APP_API_URL}/api`,
   timeout: 5000,
   headers: { 'Content-type': 'application/json' },
 })
 
+// Adds fresh access token on requests
 client.interceptors.request.use(
   async config => {
-    config.headers.authorization = `Bearer ${await firebase.auth().currentUser.getIdToken()}`
-    return config
-  },
-  error => {
-    console.log(error)
-    return Promise.reject(error)
-  }
-)
-
-clientNoAuth.interceptors.request.use(
-  async config => {
-    console.log(config)
-    return config
+    const newConfig = { ...config }
+    newConfig.headers.authorization = `Bearer ${await firebase.auth().currentUser.getIdToken()}`
+    return newConfig
   },
   error => {
     console.log(error)
